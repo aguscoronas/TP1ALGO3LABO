9977a243b796bc8500b2e0627549be5d48cc51fe
Copaitor <agus.coronas@gmail.com>
2020-04-24 13:32:45 -0300

    FB no usa más nroElegidos

--- fuerzaBruta.cpp
+++ fuerzaBruta.cpp
@@ -10,19 +10,19 @@ std::vector<int> resistencias;
  *resMax es la resistencia que queda en todo momento tanto del tuvo como de los elementos
  *elejidos que están abajo y pueden ser aplastados.
  *indice es el elemento que estamos considerando
- *nroElegidos es la cantidad de elementos que estan en el tuvo
  */
-int resuelveFuerzaBruta(int resMax, int indice, int nroElegidos){
+int resuelveFuerzaBruta(int resMax, int indice){
     //caso base: no hay mas elementos para considerar
-    if(indice == nroElementos) return nroElegidos;
+    if(indice == nroElementos) return 0;
     //caso elijo el elemento: decremento la resistencia y aumento el nro de elegidos
-    int loElijo = resuelveFuerzaBruta(std::min(resMax - pesos[indice], resistencias[indice]),
-                                      indice+1, nroElegidos+1);
+    int loElijo = 1;
+    loElijo += resuelveFuerzaBruta(std::min(resMax - pesos[indice], resistencias[indice]),
+                                      indice+1);
     //si la resistencia daba negativa, entonces nunca lo podría haber elegido así que lo anulo
     if(resMax - pesos[indice] < 0) loElijo = 0;
     
     //comparo el resultado con no haberlo elegido 
-    return std::max(loElijo, resuelveFuerzaBruta(resMax, indice+1, nroElegidos));
+    return std::max(loElijo, resuelveFuerzaBruta(resMax, indice+1));
 }
 
 int fuerzaBruta(int n, int R, const std::vector<int> &w, const std::vector<int> &r){
@@ -30,5 +30,5 @@ int fuerzaBruta(int n, int R, const std::vector<int> &w, const std::vector<int> 
     pesos = w;
     resistencias = r;    
     //valores iniciales para la recursion
-    return resuelveFuerzaBruta(R, 0, 0);
+    return resuelveFuerzaBruta(R, 0);
 }
